59a0470756a1d9c98840623bfd16bb131e92aedf
for-alisia <for-alisia@yandex.ru>
2015-12-22 19:42:15 +0300

    gulp for br-sync, jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,37 @@
-var gulp=require("gulp"),
-    browserSync=require('browser-sync');
+var gulp = require("gulp"),
+    browserSync = require('browser-sync'),
+    jade = require('gulp-jade');
 
+//Pathes for tasks
+var path = {
+    jadePath: './app/_dev/markups/pages/*.jade',
+    baseDir: './app'
+};
+
+//Jade task from app/markups/pages to app
+gulp.task('jade', function() {
+    var YOUR_LOCALS = {};
+    gulp.src('./app/_dev/markups/pages/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS,
+            pretty: '\t'
+        }))
+        .pipe(gulp.dest('./app/'))
+});
+
+//browser-sync server task for app
 gulp.task('server',function(){
     browserSync({
         port:9000,
         online:true,
         server:{
-            baseDir:'app'
-
+            baseDir: baseDir
         }
 
     });
 });
 
+//watch task for browser-sync
 gulp.task('watch',function(){
     gulp.watch([
         'app/*html',
@@ -21,4 +40,10 @@ gulp.task('watch',function(){
     ]).on('change', browserSync.reload);
 });
 
-gulp.task('default',['server','watch']);
+//watch task for jade
+gulp.task('jadeWatch', function() {
+   gulp.watch(path.jadePath, ['jade']);
+});
+
+//default task
+gulp.task('default',['jadeWatch','server','watch']);
